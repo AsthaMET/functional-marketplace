5b38e3ee7aa647c7e4e31455d8325c096269f0ac
AsthaMET <aastham.encircle@gmail.com>
2026-01-01 19:31:18 +0530

    feat: add mobile screen menu

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,11 @@
-
+// Nevigation menu
+const menuButton = document.getElementById("menu-button");
+const menu = document.getElementById("menu");
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+document.addEventListener("click", (event) => {
+  if (!menuButton.contains(event.target) && !menu.contains(event.target)) {
+    menu.classList.add("hidden");
+  }
+});
