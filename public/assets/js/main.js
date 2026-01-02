b4cd0ce4c70545e801f09327e23ac962f3e319c9
Astha <aastham.encircle@gmail.com>
2026-01-02 19:32:47 +0530

    feat: add header, add announcement-bar , add hero section

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,57 @@
-// Nevigation menu
+// menu open-close
 const menuButton = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
-menuButton.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-});
-document.addEventListener("click", (event) => {
-  if (!menuButton.contains(event.target) && !menu.contains(event.target)) {
-    menu.classList.add("hidden");
+const closeMenuButton = document.getElementById("close-menu-button");
+function openMenu() {
+  menu.style.display = "block";
+}
+function closeMenu() {
+  menu.style.display = "none";
+}
+menuButton.addEventListener("click", openMenu);
+closeMenuButton.addEventListener("click", closeMenu);
+
+// fixed header
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("#header");
+  const scrollPosition = window.scrollY;
+  const triggerPoint = 20;
+  if (scrollPosition > triggerPoint) {
+    header.classList.add("fixed-header");
+  } else {
+    header.classList.remove("fixed-header");
   }
+});
+
+// fixed announcement bar
+window.addEventListener("scroll", function () {
+  const announce = document.querySelector(".announcement-bar");
+  const scrollPosition = window.scrollY;
+  const triggerPoint = 20;
+  if (scrollPosition > triggerPoint) {
+    announce.classList.add("fixed-announcement-bar");
+  } else {
+    announce.classList.remove("fixed-announcement-bar");
+  }
+});
+
+// fixed mobile-menu
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("#menu");
+  const scrollPosition = window.scrollY;
+  const triggerPoint = 20;
+  if (scrollPosition > triggerPoint) {
+    header.classList.add("mobile-menu");
+  } else {
+    header.classList.remove("mobile-menu");
+  }
+});
+
+// mobile menu animation
+function toggleMenu() {
+  menu.classList.toggle('animated');
+}
+menuButton.addEventListener("click", toggleMenu);
+closeMenuButton.addEventListener("click", function() {
+  menu.classList.remove('animated');
 });
